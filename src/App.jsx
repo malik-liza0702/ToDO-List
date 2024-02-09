bca62f84405747bf5b8d1323db45d7fe3b61eac4
malik-liza <129332525+malik-liza@users.noreply.github.com>
2024-02-09 07:44:49 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import "./App.css";
+import {useState} from 'react';
+export default function App() {
+  const [title,setTitle]=useState("");
+  const [descp,setDescp]=useState("");
+  const [mainTask,setMainTask]=useState([]);
+  function changeHandler1(e){
+    setTitle(e.target.value);
+    
+  }
+  function changeHandler2(e){
+    
+    setDescp(e.target.value);
+  }
+  
+  function submitHandler(e){
+    e.preventDefault();
+    setMainTask([...mainTask,{title,descp}]);
+    setTitle("");
+    setDescp("");
+    console.log(mainTask);
+    
+  }
+  function deleteHandler(i){
+    let copyTask=[...mainTask];
+    copyTask.splice(i,1);
+    setMainTask(copyTask);
+  }
+  let renderTask=<h2>No task available</h2>;
+  if(mainTask.length>0){
+
+     renderTask=mainTask.map((t,i)=>{
+       return <li key={i}>
+         <div className="flex  justify-between">
+          <h5>{t.title}</h5>
+          <h6>{t.descp}</h6>
+            <button className="bg-black-400" onClick={()=>{
+         deleteHandler(i)
+            }}>Delete</button>
+          </div>
+       </li>
+                
+         
+     })
+  }
+  
+  return (
+    <main>
+      <div className="bg-black text-white" >My Todo List</div>
+      <form onSubmit={submitHandler}>
+        <input className="border-black border-8"type="text" placeholder="Enter your Task"
+          value={title}
+          onChange={changeHandler1}></input>
+         <input className="border-black border-8"type="text" placeholder="Enter your description"
+           value={descp}
+           onChange={changeHandler2}></input>
+           
+        <button className="border-zinc-800">Add Task</button>
+      </form>
+      <hr></hr>
+      <div>
+        <ul className="bg-blue text-white" >
+          <h2>{renderTask}</h2>
+        </ul>
+      </div>
+     
+      
+    </main>
+  );
+}
